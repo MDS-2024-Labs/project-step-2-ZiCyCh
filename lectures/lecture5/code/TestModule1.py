97d121c5ebfa801860f9434b5d1c05f85b88247f
Khalad Hasan <42752210+khalad-hasan@users.noreply.github.com>
2024-11-28 09:29:54 -0800

    added lecture 5

---
+++ lectures/lecture5/code/TestModule1.py
@@ -0,0 +1,10 @@
+import unittest
+from mod1 import addition as md1
+from mod2 import subtraction as md2
+
+class TestAdd(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(md1(1,1),2)
+        self.assertEqual(md1(1,2),3)
+     
