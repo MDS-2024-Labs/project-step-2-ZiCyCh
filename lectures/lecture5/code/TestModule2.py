f07b44263f80162bf3126844e5b71db15213988c
wtm <TianmuWang6688>
2024-12-09 11:30:49 -0800

    update test

--- lectures/lecture5/code/TestModule2.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from mod1 import addition as md1
-from mod2 import subtraction as md2
-
-class TestSub(unittest.TestCase):
-    
-    def test_subtraction(self):
-        self.assertEqual(md2(1,1),1)
-        self.assertEqual(md2(1,2),-1)
